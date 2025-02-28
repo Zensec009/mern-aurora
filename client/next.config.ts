8ce29d088b8b9add95c4a2853e17d1e80736f0d7
Dharmit Shah <200708081+Zensec009@users.noreply.github.com>
2025-02-28 08:52:37 +0530

    Fixing Build: Removed basepath

--- client/next.config.ts
+++ client/next.config.ts
@@ -4,10 +4,11 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: "export",  // Enables static site export
   distDir: "out",    // Ensures build files are placed in 'out' directory
-  basePath: process.env.NODE_ENV === "production" ? "/mern-aurora" : "",
+
   images: {
     unoptimized: true,  // Required for GitHub Pages
   }
+
 };
 
 export default nextConfig;
