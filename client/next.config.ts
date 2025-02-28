68af1631f06c1ff8cee07a2c4cc035c28a25505f
Dharmit Shah <200708081+Zensec009@users.noreply.github.com>
2025-02-28 09:46:14 +0530

    Updated: reassigned basepath

--- client/next.config.ts
+++ client/next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: "export",  // Enables static site export
   distDir: "out",    // Ensures build files are placed in 'out' directory
-  basePath: '/client',
 
   images: {
     unoptimized: true,  // Required for GitHub Pages
